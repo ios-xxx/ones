b5e43af17daf8dfc18da3eb9a4a37cd43e78d0ac
CE.Mr <ce.mr@qq.com>
2018-12-03 21:02:53 +0800

    add verification

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import Adapter from './components/utils/Adapter';
 import Storage from  './components/global/Storage';
 import NavigationPage  from './components/NavigationView/NavigationView';
 import BasePage  from './components/BasePage/BasePage';
+import Verification from './components/Verification/Verification';
 
 var One = {
- 
+
     Toast,
     Adapter,
     Storage,
     NavigationPage,
     BasePage,
-    
+    Verification,
+
 }
 
 module.exports = One;
